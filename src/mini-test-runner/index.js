c39cbea864cab5e75509ad6d1a5f2dfdc533cefa
Sixty <hiliulin@aliyun.com>
2023-04-14 23:58:39 +0800

    feat: 模拟测试框架

--- src/mini-test-runner/index.js
+++ src/mini-test-runner/index.js
@@ -5,3 +5,28 @@
 // beforeAll beforeEach afterAl afterEach
 // describe
 // 自动执行所有的测试脚本 *。spec.js
+
+const tests = [];
+export function test(name, callback) {
+  tests.push({ name, callback });
+}
+
+export const it = test;
+
+export function run() {
+  for (const test of tests) {
+    test.callback();
+  }
+}
+
+export function expect(actual) {
+  return {
+    toBe(expected) {
+      if (expected === actual) {
+        console.log("ok");
+      } else {
+        throw new Error();
+      }
+    },
+  };
+}
---
+++ src/mini-test-runner/index.spec.js
@@ -0,0 +1,11 @@
+import { test, run, it, expect } from "./index";
+
+test("first test", () => {
+  console.log("first");
+});
+
+it("first it", () => {
+  console.log("first");
+});
+
+run();
