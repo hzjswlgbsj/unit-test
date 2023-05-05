13f15c3270043de571d0c286bdaf0e602d54b796
Sixty <hiliulin@aliyun.com>
2023-05-05 23:58:02 +0800

    feat: 创建测试数据的方式

---
+++ blog/tests/helpers/todo.ts
@@ -0,0 +1,12 @@
+import { State } from "..";
+
+export function createTodo(
+  title: string,
+  content: string = "这是一个 todo 的内容"
+) {
+  return {
+    title,
+    content,
+    state: State.active,
+  };
+}
