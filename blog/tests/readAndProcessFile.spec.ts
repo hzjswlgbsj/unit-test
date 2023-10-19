8ad85dfcab0ce60ce8738630ce85502525bc9f47
Sixty <hiliulin@aliyun.com>
2023-10-20 00:16:27 +0800

    feat: 单元测试,类的依赖注入

--- blog/tests/readAndProcessFile.spec.ts
+++ blog/tests/readAndProcessFile.spec.ts
@@ -1,8 +1,44 @@
 import { it, expect, describe } from "vitest";
-import { readAndProcessFile } from "./readAndProcessFile";
+import { readAndProcessFile, FileReader } from "./readAndProcessFile";
 
 describe("di function", () => {
   it("read and process file", () => {
+    class StubFileReader {
+      read(filePath: string) {
+        return "sixty";
+      }
+    }
+
+    const result = readAndProcessFile(".test", new StubFileReader());
+    expect(result).toBe("sixty->test unit");
+  });
+});
+
+describe("di class", () => {
+  it("构造函数", () => {
+    class StubFileReader implements FileReader {
+      read(filePath: string) {
+        return "sixty";
+      }
+    }
+
+    const result = new readAndProcessFile(".test", new StubFileReader());
+    result.fileReader = new StubFileReader();
+    expect(result.run()).toBe("sixty->test unit");
+  });
+
+  it("属性", () => {
+    class StubFileReader implements FileReader {
+      read(filePath: string) {
+        return "sixty";
+      }
+    }
+
+    const result = new readAndProcessFile(".test", new StubFileReader());
+    result.fileReader = new StubFileReader();
+    expect(result.run()).toBe("sixty->test unit");
+  });
+  it("方法", () => {
     class StubFileReader {
       read(filePath: string) {
         return "sixty";
